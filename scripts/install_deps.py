4cc0bc581da497848e1e7269f4d49f496aa910c0
Nandeeka Nayak <nandeeka@gmail.com>
2023-09-12 09:28:29 -0500

    Add instructions to produce docker-compose.yaml

--- scripts/install_deps.py
+++ scripts/install_deps.py
@@ -1,10 +1,22 @@
+import os
+import sys
+sys.path.insert(0, "..")
+
 import shutil
 
 import scripts.yaml as yaml
 
 def main():
-    shutil.copy("../docker-compose.yaml.template",
-        "../docker-compose.yaml")
+    dc_fn = "../docker-compose.yaml"
+    shutil.copy(dc_fn + ".template", dc_fn)
+
+    uid = os.getuid()
+    gid = os.getgid()
+
+    docker_compose = yaml.load(dc_fn)
+    docker_compose["services"]["cl"]["environment"][0] = "USER_UID=" + str(uid)
+    docker_compose["services"]["cl"]["environment"][1] = "USER_GID=" + str(gid)
+    yaml.dump(docker_compose, dc_fn)
 
 if __name__ == "__main__":
     main()
